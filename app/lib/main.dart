01cd729709651e3d0043cceb7a2072295d39c134
viktorkanuta-a11y <viktorkanuta@gmail.com>
2026-09-18 18:32:03 +0500

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,13 +2,40 @@ import 'package:flutter/material.dart';
 
 import 'replay_home.dart';
 import 'splash_screen.dart';
+import 'upscale.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const RePlayApp());
 }
 
-class RePlayApp extends StatelessWidget {
+class RePlayApp extends StatefulWidget {
   const RePlayApp({super.key});
+
+  @override
+  State<RePlayApp> createState() => _RePlayAppState();
+}
+
+class _RePlayAppState extends State<RePlayApp> {
+  late final AppLifecycleListener _listener;
+
+  @override
+  void initState() {
+    super.initState();
+    // Закрытие окна: остановить движок и отпустить запрет сна
+    _listener = AppLifecycleListener(
+      onExitRequested: () async {
+        stopUpscale();
+        return AppExitResponse.exit;
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _listener.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
